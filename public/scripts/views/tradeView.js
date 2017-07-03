351f16a4726e3ec8206c0911b7b934efb0bbdd9d
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-02 21:36:13 -0700

    Added handlebar functions for dropdown menu

--- public/scripts/views/tradeView.js
+++ public/scripts/views/tradeView.js
@@ -10,5 +10,17 @@ var app = app || {};
     return template(player);
   };
 
+  tradeView.populateLeagues = function(league) {
+    let template = handlebars.compile('#league-template');
+    return template(league);
+  };
+
+  tradeView.teamLeagues = function(team) {
+    let template = handlebars.compile('#team-template');
+    return template(team);
+  };
+
+  
+
   module.tradeView = tradeView;
 })(app);
