393a8686e5e7cc6fdf4c7d19f879ba589eff540a
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-03 15:59:54 -0700

    Added token to token.js and added token.js in .gitignore

---
+++ public/scripts/controllers/token.js
@@ -0,0 +1,3 @@
+'use strict';
+
+let token = 'bE13U1JHOW5McVhjRmdvVmw2bTdnTGdPSjpSblhGYVBwNThLR01UYkZBbE9Sa1I3Rlh6WXVlRTdtcndpaERuendXcjJmSVpvZXo4eg=='
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const requestProxy = require('express-request-proxy');
 const fs = require('fs');
 const pg = require('pg');
 const bcrypt = require('bcrypt');
@@ -9,11 +10,22 @@ const app = express();
 const PORT = process.env.PORT ||3000;
 const saltRounds = 10;
 
-// const connString = process.env.'I cant remember';
-// const client = new pg.Client(connString);
-//
-// client.connect();
-// client('error', (err) => console.log(err));
+var proxyTwitter = function(request, response) {
+  (requestProxy({
+    url: 'https://api.twitter.com/1.1/search/tweets.json?q=%40bacon'
+    json: true,
+    headers: {
+      'Authorization': 'Bearer <token>'
+    }
+  }))(request, response);
+};
+
+
+const connString = process.env.DB;
+const client = new pg.Client(connString);
+
+client.connect();
+client('error', (err) => console.log(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -27,25 +39,27 @@ app.get('/validate', (req, res) => {
   client.query('SELECT username FROM tablename WHERE username=$1',
   [req.body.username])
   .then(result => {
-    res.body.available = result.rows.length === 0 ?  false : true;
+    res.body.available = !result.rows.length === 0;
   })
   .catch(err => console.log(err));
 });
 
-app.get('/password', (req, res)=> {
-
-  client.query('SELECT hash FROM tablename WHERE username=$1',
+app.get('/password', (req, res)=> {client.query('SELECT hash FROM tablename WHERE username=$1',
   [req.body.username])
   .then(result => {
     bcrypt.compare(req.password, result.row[0].hash, (err, valid) => {
       res.body.valid  = valid ? result.row[0] : false;
     });
+  });
 });
 
 app.post('new_profile', (req, res) => {
   bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    if(err) {
+      console.log(err);
+    }
     req.body.hash = hash});
-  client.query('INSERT INTO table(first, last, email, userName, hash) VALUES($1, $2, $3, $4, $5, $6) ON CONFLICT DO NOTHING',
+  client.query('INSERT INTO table(first, last, email, userName, hash) VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING',
   [req.body.first, req.body.last, req.body.email, req.body.userName, req.body.hash],
   (err) => {
     if(err) {
@@ -54,31 +68,31 @@ app.post('new_profile', (req, res) => {
   });
 });
 
-app.get('/trades', (req, res) => {
-  fs.readFile('./public/scripts/models/sampleJSON.json', (err, data) => {
-    res.send(JSON.parse(data.toString()));
-  });
-
-  // client.query(sqlReq)
-  // .then(result => res.send(result.rows))
-  // .catch(console.error);
-});
-
-app.get('/trades/league', (req, res) => {
-  let sqlReq = 'Some more shit';
-
-  client.query(sqlReq)
-  .then(result => res.send(result.rows))
-  .catch(console.error);
-});
-
-app.get('/trades/team', (req, res) => {
-  let sqlReq = 'EVEN MORE SHIT!!!';
-
-  client.query(sqlReq)
-  .then(result => res.send(result.rows))
-  .catch(console.error);
-});
+// app.get('/trades', (req, res) => {
+//   fs.readFile('./public/scripts/models/sampleJSON.json', (err, data) => {
+//     res.send(JSON.parse(data.toString()));
+//   });
+//
+//   // client.query(sqlReq)
+//   // .then(result => res.send(result.rows))
+//   // .catch(console.error);
+// });
+//
+// app.get('/trades/league', (req, res) => {
+//   let sqlReq = 'Some more shit';
+//
+//   client.query(sqlReq)
+//   .then(result => res.send(result.rows))
+//   .catch(console.error);
+// });
+//
+// app.get('/trades/team', (req, res) => {
+//   let sqlReq = 'EVEN MORE SHIT!!!';
+//
+//   client.query(sqlReq)
+//   .then(result => res.send(result.rows))
+//   .catch(console.error);
+// });
 
 
 app.listen(PORT, () => console.log('Server Active!'));
