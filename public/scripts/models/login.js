c4cb8cfbcc1a8033f58f21aa495fda4ea35027f8
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-03 14:35:13 -0700

    Added a hash property to User object that runs password through bcrypt

--- public/scripts/models/login.js
+++ public/scripts/models/login.js
@@ -5,8 +5,24 @@ var app = app||{};
 (function(module) {
 
   const login = {};
+  let $login = $('#login');
 
-  
+  login.verifyUser = (e) => {
+    let attempt = {};
+    e.preventDefault();
+    attempt.userName = $login.find('input').first().val();
+    attempt.passWord = $login.find('input').last().val();
+    $.get('/validate', attempt)
+    .then(res => {
+      !res ? alert('User name does not exist!') :
+    });
+  }
+
+  login.verifyPass = (password) => {
+    $.get('/password', password).then(callback);
+  }
+
+
 
 
   module.login = login;
