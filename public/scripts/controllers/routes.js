d787ceb4db50065c83c1f0590918b1307d8bf6b0
sharmarke <smarky292@gmail.com>
2017-07-05 13:21:37 -0700

    login page complete

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
-page('/', app.homeController.init);
+page('/home', app.homeController.init);
 page('/login', app.loginController.init)
-page('/about', app.aboutController.init);
+// page('/about', app.aboutController.init);
 
 page();
