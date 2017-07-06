85bc62ba756b4ba9a338f71f9d99df243b3446ed
sharmarke <smarky292@gmail.com>
2017-07-06 10:27:55 -0700

    about page

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -4,6 +4,6 @@ page('/', app.homeController.init);
 page('/home', app.homeController.init);
 
 page('/login', app.loginController.init)
-// page('/about', app.aboutController.init);
+page('/about', app.aboutController.init);
 
 page();
