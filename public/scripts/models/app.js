c9de8bcbcd24647f8e5f7112260689f026fa3d61
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-06 11:33:31 -0700

    Fixed bug in ind-tweets rendering

--- public/scripts/models/app.js
+++ public/scripts/models/app.js
@@ -98,7 +98,7 @@ function fetchTweets(callback) {
   .then($.get(`/search/tweets.json?q=from%3A%40${twitTwo}%20%40${twitOne}&src=typd`).then(data => {
     twitTwoObj.tweets = data.statuses;
     app.Tweets.second = data.statuses;
-  }).then(callback).then(() => app.tweets.runBoth));
+  }).then(callback));
 }
 
 function assignTwits(twitOne, twitTwo){
@@ -125,4 +125,5 @@ function results(){
   sortActiveTweets();
   expandAndCenter();
   renderResults();
+  app.Tweets.runBoth();
 }
