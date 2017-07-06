7566f351abf879acd0d683e0f2b9605eb943fe12
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-06 11:26:40 -0700

    Added accodrind dropdown menu

--- public/scripts/hamburger.js
+++ public/scripts/hamburger.js
@@ -1,5 +1,11 @@
 'use strict';
 
+$('#ind-tweets').hide();
+
 $('nav img').on('click', () => {
   $('nav ul').slideToggle(350);
+});
+
+$('#accordion').on('click', function() {
+  $('#ind-tweets'.slideToggle('fast'));
 });
